a2e9ad0f7f285dd974d027a326bfb7ed4f66232b
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-09 10:12:13 -0700

    done

--- task1.py
+++ task1.py
@@ -73,3 +73,42 @@ Buster dog
 Shih-tzu is owned by Christy
 (10 points) 
 """
+class pet:
+    animal= ''
+    breed= ''
+    name= ''
+    owner= ''
+    birthdate= ''
+
+    def __init__(self):
+        self.animal= input("Type of animal? : ")
+        self.vreed= input("What kind of breed? : ")
+        self.name = input("Enter animal name: ")
+        self.owner = input("Enter your name: ")
+        self.birthdate = input("Enter animal birthdate(in the form of year-month-day): ")
+    
+    def display(self):
+        print(self.name+''+self.animal)
+        print(self.breed+' is owned by '+self.owner)
+
+pets=[]
+
+i=0
+while True:
+    print('1. Enter a new pet')
+    print('2. Retrieve a pet')
+    print('3. Exit')
+    a=int(input('').strip())
+    if a==1:
+        pets.append(pet())
+        continue
+    elif a==2:
+        b=input("which pet? : ")
+        for i in pets:
+            index=pets.index(i)
+            if b == i.name:
+                pets[index].display()
+                break
+    else:
+        break
+    
